e421c9ce35e26cf2537429a454b4b541668e27bb
MidhileshMajety <midhileshmajeyti@gmail.com>
2018-04-22 15:39:07 +0530

    nthng

---
+++ app/src/main/java/com/milli/ontime/StudentCheckRequest.java
@@ -0,0 +1,13 @@
+package com.milli.ontime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentCheckRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_check_request);
+    }
+}
